4e7ae401f0777c3a3446d5848bf9b382e536e12c
Krunal Shah <shahkrunal7091@gmail.com>
2022-02-16 22:01:26 +0530

    feat: setup stack

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+createInertiaApp({
+  resolve: (name) => require(`./pages/${name}`),
+  setup({ el, App, props }) {
+    render(<App {...props} />, el)
+  },
+})
